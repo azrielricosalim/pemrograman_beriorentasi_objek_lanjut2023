de94f8719bcfa036873b9d3065ff367d1ad7b29a
azrielricosalim <133220423+azrielricosalim@users.noreply.github.com>
2023-06-24 20:32:59 +0700

    Add files via upload

---
+++ tugas7/Meta Programming menghitung BMI.py
@@ -0,0 +1,21 @@
+#Nama   : Sandy Dwi Julian
+#Kelas  : K1
+#NIM    : 210511033
+
+class MenghitungBMIMeta(type):
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        def pria(cls, tinggi):
+            return (tinggi - 100) - (tinggi - 100) * 10/100
+        cls.pria = classmethod(pria)
+
+        def wanita(cls, tinggi):
+            return (tinggi - 100) - (tinggi - 100) * 15/100
+        cls.wanita = classmethod(wanita)
+class Ideal(metaclass=MenghitungBMIMeta):
+    pass
+A = Ideal()
+B = A.pria(173)
+C = A.wanita(155)
+print('BMI Pria:',B)
+print('BMI Wanita:',C)
