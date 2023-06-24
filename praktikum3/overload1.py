5e709e73b2394130a636c1b2094c4eda66d1d3a3
azrielricosalim <133220423+azrielricosalim@users.noreply.github.com>
2023-06-24 17:26:03 +0700

    Add files via upload

---
+++ praktikum3/overload1.py
@@ -0,0 +1,3 @@
+print(min(100, 2, 5, 100, 3000, 50)) 
+print(min([1, 2, 3, 4, 0])) 
+print(min("Zebra")) 
---
+++ praktikum3/overload2.py
@@ -0,0 +1,7 @@
+a = [100, 5, 3]
+a.sort()
+print(a) 
+
+b = ["z", "d", "h"]
+b.sort()
+print(b) 
